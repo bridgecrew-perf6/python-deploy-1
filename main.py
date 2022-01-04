918e071ecb36fd81ff788bfcfb1d80496ff50a7b
Gabriel Andrade <gandradevini@gmail.com>
2022-01-04 12:04:27 -0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return "Se indiano consegue deployar, nois tb"
+
+if __name__ == "__main__":
+    app.secret_key = 'ItIsASecret'
+    app.debug = True
+    app.run()
